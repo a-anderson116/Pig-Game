04179b038d0c1f7f895ecb0141618dcbd3d45c20
Alex Anderson <andersonaj116@gmail.com>
2022-11-03 09:46:25 -0500

    Initial Commit

---
+++ Pig-Game/Pig_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pig_GameApp.swift
+//  Pig-Game
+//
+//  Created by Alex Anderson on 11/3/22.
+//
+
+import SwiftUI
+
+@main
+struct Pig_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
